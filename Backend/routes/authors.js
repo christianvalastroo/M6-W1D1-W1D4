6c40b467547ad966330228c7a678b016f95ceaa7
Christian Valastro <christianvalastro@icloud.com>
2026-06-03 22:24:52 +0200

    Aggiunta endpoint GET autori

--- Backend/routes/authors.js
+++ Backend/routes/authors.js
@@ -1,9 +1,18 @@
 const express = require("express")
+const Author = require("../models/Author")
 
 const authorsRouter = express.Router()
 
-authorsRouter.get("/", (req, res) => {
-    res.send("Lista autori")
+authorsRouter.get("/", async (req, res) => {
+    try {
+        const authors = await Author.find()
+
+        res.status(200).send(authors)
+    } catch (error) {
+        res.status(500).send({
+            message: "Errore nel recupero degli autori"
+        })
+    }
 })
 
 module.exports = authorsRouter
